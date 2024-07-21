eb286caf55d63961ad7842cbe9f71bc099bad16d
nivya <nivya.mathew@vonnue.com>
2024-07-21 13:49:32 +0530

    refactor: removed commented lines

--- CS50-C/meow.c
+++ CS50-C/meow.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-// int main(void)
-// {
-//     int i = 3;
-//     while (i > 0)
-//     {
-//         printf("meow\n");
-//         i--;
-//     }
-// }
-
 void meow(int n);
 
 int main(void)
